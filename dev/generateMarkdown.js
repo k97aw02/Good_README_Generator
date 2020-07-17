480077394322565266436e5a86cc7071208d84eb
k97aw02 <k97aw02@gmail.com>
2020-07-17 09:27:11 -0500

    update index.js

---
+++ dev/generateMarkdown.js
@@ -0,0 +1,40 @@
+function generateMarkdown(data) {
+    return `
+    
+    # ${data.title}
+    
+    ## Description 
+    
+    ${data.description}
+    
+    ## Table of Contents
+    * [Installation](#-Installation)
+    * [Usage](#-Usage)
+    * [Credits](#-Credits)
+    * [License](#-License)
+    * [Badges](#-Badges)
+    * [Authors](#-Authors)
+    * [Contributing](#-Contributing)
+    * [Tests](#-Tests)
+    
+    ## Installation
+    ${data.install}
+    ## Usage
+    ${data.usage}
+    ## License
+    ${data.license}
+    ## Authors
+    ${data.author}
+    ## Contributing
+    ${data.contribute}
+    ## Tests
+    ${data.test}
+    ## Questions
+    
+    ${data.questions}
+    email address: ${data.email}
+    github: [${data.githubUserName}](https://github.com/${data.githubUserName})
+    `;
+  };
+  
+  module.exports = generateMarkdown;
